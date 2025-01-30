fdcabe74e6194b1cf8f5bae054cb930651f9efb7
ghst94 <127649067+ghst94@users.noreply.github.com>
2025-01-30 23:45:31 +0000

    Fix CORS and environment variables

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    projectConfig: {
-      database_url: process.env.DATABASE_URL || "postgres://localhost:5432/medusa",
-      redis_url: process.env.REDIS_URL || "redis://localhost:6379",
-      store_cors: process.env.STORE_CORS || "http://localhost:8000",
-      admin_cors: process.env.ADMIN_CORS || "http://localhost:7000",
-    },
-    plugins: [],
-  };
-  
+  projectConfig: {
+    database_url: process.env.DATABASE_URL,
+    redis_url: process.env.REDIS_URL,
+    store_cors: process.env.STORE_CORS || "http://localhost:8000",
+    admin_cors: process.env.ADMIN_CORS || "http://localhost:7000",
+  },
+  plugins: [],
+};
+
